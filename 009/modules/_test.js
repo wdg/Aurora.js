78ffa24a70712aaf37b8d543f70408a4eb5a3030
Wesley de Groot <wes@vista.aero>
2018-07-31 21:55:30 +0200

    _.js Auto Commit [v0.1.0 r180731] #22

--- 009/modules/_test.js
+++ 009/modules/_test.js
@@ -7,7 +7,7 @@
                     _   | |  \___  \
      ______    _   | |__| |  ____) |
     |______|  (_)   \____/  |______/
-                              v0.0.7
+                              v1.0.0
 
     https://www.github.com/wesdegroot/_.js/
     or https://www.wdgwv.com
@@ -17,15 +17,13 @@
     Licence: https://github.com/wesdegroot/_.js/blob/master/LICENCE.md (CC BY 4.0)
     Latest:  https://raw.githubusercontent.com/wesdegroot/_.js/master/latest/_.js
 */
+
+console.log('Extension _test.js loaded :)')
+
 if (typeof _ !== 'function') {
   window.alert('Please make sure _.js is loaded!')
 } else {
-  // Load Text functions
-  window._.fn.text_to_console = function (name) {
-    var len = this.length
-
-    while (len--) {
-      console.log('Text to console: ' + this[len].innerHTML)
-    }
-  }
+  console.log('Found _.js')
 }
+
+console.log('Goodbye!')
